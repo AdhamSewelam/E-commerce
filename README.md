19e00bf8131af44ad46f964a20430b46dd897e23
Adham Sewelam <adham.h.sewelam@gmail.com>
2022-12-21 23:09:50 +0200

    Seed Sample Users

--- README.md
+++ README.md
@@ -82,3 +82,7 @@
     3. create seed route
     4. use route in server.js
     5. seed sample product
+19. Seed Sample Users
+    1. create user model
+    2. seed sample users
+    3. create user routes
--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Adham',
+      email: 'adhamsewelam123@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Roman',
+      email: 'roman@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
