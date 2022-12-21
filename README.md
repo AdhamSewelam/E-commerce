c4e686cbe3311c88037e225b6dadf9e9ae04d654
Adham Sewelam <adham.h.sewelam@gmail.com>
2022-12-22 00:05:43 +0200

    Create SignIn backend API

--- README.md
+++ README.md
@@ -86,3 +86,7 @@
     1. create user model
     2. seed sample users
     3. create user routes
+20. Create SignIn backend API
+     1. create signin api
+     2. npm i jsonwebtoken
+     3. define generateToken
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 dotenv.config();
 
 mongoose
@@ -18,11 +19,17 @@ mongoose
 
 const app = express();
 
-app.use('/api/seed', seedRouter);
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
