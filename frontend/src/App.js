707f9b9a8f9d8880e1d0c640f52cb077525c9fef
Adham Sewelam <adham.h.sewelam@gmail.com>
2022-12-22 15:19:12 +0200

    Create Shipping Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -75,6 +76,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/shipping" element={<ShippingScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
